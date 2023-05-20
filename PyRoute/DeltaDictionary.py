f814d2705930d6c97432cda41ea208e794561846
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-05-21 03:10:32 +1000

    Add load_from_traveller_map method to SectorDictionary
    This commit follows the lead of the Star object's
    static parse_line_into_star method to provide an easy
    way to turn an on-disk sector file into an in-memory
    SectorDictionary, ready for use in delta debugging.

--- PyRoute/DeltaDictionary.py
+++ PyRoute/DeltaDictionary.py
@@ -3,6 +3,14 @@ Created on May 21, 2023
 
 @author: CyberiaResurrection
 """
+import codecs
+import logging
+import os
+import string
+from pathlib import Path
+
+from PyRoute.Galaxy import Sector
+from PyRoute.Star import Star
 
 
 class DeltaDictionary(dict):
@@ -23,6 +31,7 @@ class SectorDictionary(dict):
         super().__init__()
         self.name = name
         self.filename = filename
+        self.position = None
 
     def update(self, __m, **kwargs):
         for key in __m:
@@ -33,10 +42,85 @@ class SectorDictionary(dict):
         assert isinstance(value, SubsectorDictionary), "Values must be SubsectorDictionary objects"
         super().__setitem__(item, value)
 
+    @property
+    def lines(self):
+        result = list()
+        for sub_name in self.keys():
+            result.extend(self[sub_name].items)
+
+        return result
+
+    @staticmethod
+    def load_traveller_map_file(filename):
+        basename = os.path.basename(filename)
+        lines = None
+
+        # read travellermap file in, line by line
+        with codecs.open(filename, 'r', 'utf-8') as infile:
+            lines = [line for line in infile]
+
+        nameline = lines[3]  # assuming the definitive name line is the 4th line in what got read in
+        name = nameline.strip('#')
+        position = lines[4]
+
+        sector = SectorDictionary(name.strip(), basename)
+        headers, starlines = SectorDictionary._partition_file(lines)
+        sector.headers = headers
+        sector.position = position.strip()
+
+        # dig out subsector names, and use them to seed the dict entries
+        sublines = [line for line in headers if '# Subsector ' in line]
+        subsector_names = dict()
+
+        for line in sublines:
+            bitz = line.split(':')
+            alpha = bitz[0][-1]
+            subname = bitz[1].strip()
+            subsector_names[alpha] = subname
+            subsec = SubsectorDictionary(subname)
+            sector[subname] = subsec
+
+        # now subsectors are seeded, run thru the elements of starlines and deal them out to their respective subsector
+        # dicts
+        dummy = Sector('', '# 0,0')
+        logging.disable(logging.WARNING)
+        for line in starlines:
+            # Re-use the existing, battle-tested, validation logic rather than scraping something new and buggy together
+            star = Star.parse_line_into_star(line, dummy, 'scaled', 'scaled')
+            if not star:
+                continue
+            subsec = star.subsector()
+            subname = subsector_names[subsec]
+            sector[subname].items.append(line)
+
+        return sector
+
+    @staticmethod
+    def _partition_file(lines):
+        """
+            Break lines out into headers section, which is retained, and starlines, which gets minimised later on
+            - this assumes downloaded-from-TravellerMap sector file
+        """
+        headers = []
+        starlines = []
+        isheader = True
+        for line in lines:
+            if isheader:
+                headers.append(line)
+                if line.startswith('----'):
+                    isheader = False
+            else:
+                starlines.append(line)
+        return headers, starlines
+
 
 class SubsectorDictionary(dict):
 
     def __init__(self, name):
         self.name = name
+        self.items = list()
         super().__init__()
 
+    @property
+    def lines(self):
+        return self.items
--- Tests/testDeltaDictionary.py
+++ Tests/testDeltaDictionary.py
@@ -1,5 +1,7 @@
+import codecs
 import unittest
 import sys
+from pathlib import Path
 
 from PyRoute.DeltaDictionary import DeltaDictionary, SectorDictionary, SubsectorDictionary
 
@@ -96,6 +98,34 @@ class testSectorDictionary(unittest.TestCase):
         self.assertEqual(1, len(foo.keys()), "Target sector dictionary should have one key")
         self.assertEqual('name', foo.name)
         self.assertEqual('filename', foo.filename)
+
+    def test_load_from_traveller_map_file(self):
+        sourcefile = 'DeltaFiles/Dagudashaag-spiked.sec'
+
+        # load_traveller_map_file is a little slow as it uses Star's parse_line_into_star method
+        # to validate the input line
+        sector = SectorDictionary.load_traveller_map_file(sourcefile)
+        self.assertTrue(isinstance(sector, SectorDictionary), "SectorDictionary object not created")
+
+        self.assertEqual('Dagudashaag-spiked.sec', sector.filename)
+        self.assertEqual('Dagudashaag', sector.name)
+        self.assertEqual('# -1,0', sector.position)
+        self.assertTrue(isinstance(sector.headers, list), 'Sector headers not set after load')
+        self.assertEqual(41, len(sector.headers), "Unexpected header length after load")
+        self.assertEqual(16, len(sector.keys()), "Sector dictionary should have 16 subsector dicts")
+        self.assertEqual(561, len(sector.lines), "Unexpected number of star lines after load")
+
+        # now check size of each subsector
+        sub_sizes = {'Mimu': 37, 'Old Suns': 32, 'Arnakhish': 37, 'Iiradu': 33, 'Shallows': 40, 'Ushra': 31,
+                     'Khandi': 28, 'Kuriishe': 33, 'Zeda': 38, 'Remnants': 38, 'Pact': 40, 'Gadde': 37, 'Bolivar': 29,
+                     'Argi': 34, 'Sapphyre': 37, 'Laraa': 37}
+
+        for subname in sub_sizes:
+            self.assertEqual(
+                sub_sizes[subname],
+                len(sector[subname].lines),
+                "Unexpected number of star lines in " + subname + " subsector after load"
+            )
 
 if __name__ == '__main__':
     unittest.main()
