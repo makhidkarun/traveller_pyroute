ea147729f57e15308a2beea62b982d138899dcaa
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-06-05 23:44:27 +1000

    Flesh out log_odd_unit method on TradeBalance

--- PyRoute/TradeBalance.py
+++ PyRoute/TradeBalance.py
@@ -1,10 +1,14 @@
+import functools
+
 from PyRoute.Galaxy import AreaItem
+from PyRoute.Star import Star
 
 
 class TradeBalance(dict):
 
-    def __init__(self, area_item=None):
+    def __init__(self, area_item=None, stat_field=None):
         assert isinstance(area_item, AreaItem), "AreaItem parm must be an instance of AreaItem"
+        assert isinstance(stat_field, str), "Stat_field must be a string"
         super().__init__()
 
         self.area_item = area_item
@@ -19,9 +23,26 @@ class TradeBalance(dict):
         self._check(item, value)
         super().__setitem__(item, value)
 
+    def log_odd_unit(self, star: Star, target: Star):
+        sector_tuple = self._balance_tuple(star.sector.name, target.sector.name)
+        if sector_tuple not in self:
+            self[sector_tuple] = 0
+        self[sector_tuple] += 1
+        if 1 < self[sector_tuple]:
+            star.sector.stats.passengers += 1
+            target.sector.stats.passengers += 1
+            self[sector_tuple] -= 2
+
     @staticmethod
     def _check(key: tuple, value: int):
         assert isinstance(key, tuple), "Key must be tuple"
         assert 2 == len(key), "Key must be 2-element tuple"
         assert isinstance(key[0], str) and isinstance(key[1], str), "Key must be 2-element tuple of strings"
         assert isinstance(value, int), "Value must be integer"
+
+    @staticmethod
+    @functools.cache
+    def _balance_tuple(name_from, name_to):
+        if name_from <= name_to:
+            return (name_from, name_to)
+        return (name_to, name_from)
--- Tests/testTradeBalance.py
+++ Tests/testTradeBalance.py
@@ -7,7 +7,7 @@ from PyRoute.TradeBalance import TradeBalance
 class testTradeBalance(unittest.TestCase):
     def test_trade_balance_add_success(self):
         item = AreaItem('name')
-        foo = TradeBalance(area_item=item)
+        foo = TradeBalance(area_item=item, stat_field='passengers')
         self.assertEqual(0, len(foo), "Empty TradeBalance should have zero elements")
         key = ('foo', 'bar')
         foo[key] = 1
