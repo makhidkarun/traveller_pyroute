40622f3e3734b82d85cc5cae2fc26c44d8b48ff5
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-06-07 23:48:23 +1000

    Check star well-formedness when reading sectors

--- PyRoute/DeltaDebug/DeltaGalaxy.py
+++ PyRoute/DeltaDebug/DeltaGalaxy.py
@@ -57,7 +57,7 @@ class DeltaGalaxy(Galaxy):
 
                     star.tradeCode.sophont_list.append("{}A".format(self.alg[star.alg_code].population))
                     star.is_redzone = self.trade.unilateral_filter(star)
-
+                    star.is_well_formed()
 
             self.sectors[sec.name] = sec
             self.logger.info("Sector {} loaded {} worlds".format(sec, len(sec.worlds)))
--- PyRoute/Galaxy.py
+++ PyRoute/Galaxy.py
@@ -334,6 +334,7 @@ class Galaxy(AreaItem):
 
                     star.tradeCode.sophont_list.append("{}A".format(self.alg[star.alg_code].population))
                     star.is_redzone = self.trade.unilateral_filter(star)
+                    star.is_well_formed()
 
             self.sectors[sec.name] = sec
             self.logger.info("Sector {} loaded {} worlds".format(sec, len(sec.worlds)))
