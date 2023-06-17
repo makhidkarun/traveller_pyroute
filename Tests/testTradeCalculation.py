e5a8d6d6bc1704f7164e9e71abfa6a5b099c6923
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-06-18 01:31:31 +1000

    Rumble route_cost b0rkage

--- Tests/testTradeCalculation.py
+++ Tests/testTradeCalculation.py
@@ -60,5 +60,29 @@ class testTradeCalculation(unittest.TestCase):
 
             self.assertEqual(2, tradecalc.route_weight(star1, star1))
 
+    def test_single_link_route_weight(self):
+        sector = Sector(' Core', ' 0, 0')
+        star1 = Star.parse_line_into_star(
+            "0103 Irkigkhan            B9C4733-9 Fl                   { 0 }  (E69+0) [4726] B     - - 123 8  Im M2 V           ",
+            sector, 'fixed', 'fixed')
+        star2 = Star.parse_line_into_star(
+            "0104 Shana Ma             E551112-7 Lo Po                { -3 } (301-3) [1113] B     - - 913 9  Im K2 IV M7 V     ",
+            Sector(' Core', ' 0, 0'), 'fixed', 'fixed')
+
+        galaxy = Galaxy(min_btn=13)
+        tradecalc = TradeCalculation(galaxy)
+
+        route = [star1, star2]
+        galaxy.stars.add_node(star1)
+        galaxy.stars.add_node(star2)
+        galaxy.ranges.add_node(star1)
+        galaxy.ranges.add_node(star2)
+        galaxy.stars.add_edge(star1, star2, distance=1,
+                              weight=10, trade=0, btn=10, count=0)
+
+        expected_weight = 10
+        actual_weight = tradecalc.route_cost(route)
+        self.assertEqual(expected_weight, actual_weight, "Route cost unexpected")
+
 if __name__ == '__main__':
     unittest.main()
