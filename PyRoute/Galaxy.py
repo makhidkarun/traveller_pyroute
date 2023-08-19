ae0f1f143eb4c842ab23b1d3f0dff15ca6a7025a
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-08-20 00:42:33 +1000

    Dedupe goof

--- PyRoute/Galaxy.py
+++ PyRoute/Galaxy.py
@@ -317,9 +317,6 @@ class Galaxy(AreaItem):
                     self.set_area_alg(star, sec.subsectors[star.subsector()], self.alg)
 
                     star.tradeCode.sophont_list.append("{}A".format(self.alg[star.alg_code].population))
-                    star.index = star_counter
-                    star_counter += 1
-                    self.star_mapping[star.index] = star
 
             self.sectors[sec.name] = sec
             self.logger.info("Sector {} loaded {} worlds".format(sec, len(sec.worlds)))
