7ecbd4a132b61f15ae011008efe1c5ca4a693064
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-09-11 04:27:42 +1000

    Start characterising hexmap

---
+++ Tests/testBase.py
@@ -0,0 +1,34 @@
+import argparse
+import tempfile
+import unittest
+
+
+class testBase(unittest.TestCase):
+    def _make_args(self):
+        args = argparse.ArgumentParser(description='PyRoute input minimiser.')
+        args.btn = 8
+        args.max_jump = 2
+        args.route_btn = 13
+        args.pop_code = 'scaled'
+        args.ru_calc = 'scaled'
+        args.routes = 'trade'
+        args.route_reuse = 10
+        args.interestingline = "Weight of edge"
+        args.interestingtype = None
+        args.maps = None
+        args.borders = 'range'
+        args.ally_match = 'collapse'
+        args.owned = False
+        args.trade = True
+        args.speculative_version = 'CT'
+        args.ally_count = 10
+        args.json_data = False
+        args.output = tempfile.gettempdir()
+        return args
+
+    def _make_args_no_line(self):
+        args = self._make_args()
+        args.interestingline = None
+
+        return args
+
