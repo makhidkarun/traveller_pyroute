86e90efd1f4d3a8b790385057fe8c8ea127affba
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-09-17 15:48:30 +1000

    Add basic retry logic to downloadsec

--- PyRoute/downloadsec.py
+++ PyRoute/downloadsec.py
@@ -17,7 +17,7 @@ def get_url(url, sector, suffix, output_dir):
         f = urllib.request.urlopen(url)
     except urllib.error.HTTPError as ex:
         print ("get URL failed: {} -> {}".format(url, ex))
-        return
+        return False
 
     encoding = f.headers['content-type'].split('charset=')[-1]
     content = f.read()
@@ -31,6 +31,7 @@ def get_url(url, sector, suffix, output_dir):
     with codecs.open(path, 'wb', 'utf-8') as out:
         out.write(ucontent)
     f.close()
+    return True
 
 
 if __name__ == '__main__':
@@ -53,10 +54,16 @@ if __name__ == '__main__':
         params = urllib.parse.urlencode(params)
         url = 'http://www.travellermap.com/api/sec?%s' % params
 
-        get_url(url, sector, 'sec', args.output_dir)
+        success = get_url(url, sector, 'sec', args.output_dir)
+        if not success:
+            print("Retrying " + sector)
+            get_url(url, sector, 'sec', args.output_dir)
         
         params = urllib.parse.urlencode({'sector': sector, 'accept': 'text/xml'})
         url = 'http://travellermap.com/api/metadata?%s' % params
-        get_url(url, sector, 'xml', args.output_dir)
+        success = get_url(url, sector, 'xml', args.output_dir)
+        if not success:
+            print("Retrying XML for " + sector)
+            get_url(url, sector, 'sec', args.output_dir)
 
         time.sleep(5)
