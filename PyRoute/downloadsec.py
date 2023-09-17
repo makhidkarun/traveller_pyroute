e2f3834eb6a692ae7a3809d5e7c8b881dede5071
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-09-17 22:19:14 +1000

    Update sector list (#59)
    * Update the official sector list from TravellerMap.
    
    There have been several additions to the K'kree space (2000 Worlds)
    and new Mongoose released sectors rimward of the Solomani Sphere.
    
    * Add basic retry logic to downloadsec
    
    * Pull URLErrors into retry scope

--- PyRoute/downloadsec.py
+++ PyRoute/downloadsec.py
@@ -16,8 +16,11 @@ def get_url(url, sector, suffix, output_dir):
     try:
         f = urllib.request.urlopen(url)
     except urllib.error.HTTPError as ex:
-        print ("get URL failed: {} -> {}".format(url, ex))
-        return
+        print("get URL failed: {} -> {}".format(url, ex))
+        return False
+    except urllib.error.URLError as ex:
+        print("get URL failed: {} -> {}".format(url, ex))
+        return False
 
     encoding = f.headers['content-type'].split('charset=')[-1]
     content = f.read()
@@ -31,6 +34,7 @@ def get_url(url, sector, suffix, output_dir):
     with codecs.open(path, 'wb', 'utf-8') as out:
         out.write(ucontent)
     f.close()
+    return True
 
 
 if __name__ == '__main__':
@@ -53,10 +57,16 @@ if __name__ == '__main__':
         params = urllib.parse.urlencode(params)
         url = 'http://www.travellermap.com/api/sec?%s' % params
 
-        get_url(url, sector, 'sec', args.output_dir)
+        success = get_url(url, sector, 'sec', args.output_dir)
+        if not success:
+            print("Retrying " + sector)
+            get_url(url, sector, 'sec', args.output_dir)
         
         params = urllib.parse.urlencode({'sector': sector, 'accept': 'text/xml'})
         url = 'http://travellermap.com/api/metadata?%s' % params
-        get_url(url, sector, 'xml', args.output_dir)
+        success = get_url(url, sector, 'xml', args.output_dir)
+        if not success:
+            print("Retrying XML for " + sector)
+            get_url(url, sector, 'sec', args.output_dir)
 
         time.sleep(5)
