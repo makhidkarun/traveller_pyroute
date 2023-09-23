968591c95b4fa6752763bcad3a11b9d1da4c2cf2
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-09-23 12:36:09 +1000

    Flesh out approx-SP distance graph

--- PyRoute/Pathfinding/ApproximateShortestPathTreeDistanceGraph.py
+++ PyRoute/Pathfinding/ApproximateShortestPathTreeDistanceGraph.py
@@ -3,8 +3,53 @@ Created on Sep 23, 2023
 
 @author: CyberiaResurrection
 """
+import numpy as np
 from ApproximateShortestPathTree import ApproximateShortestPathTree
+from DistanceGraph import DistanceGraph
+from single_source_dijkstra import implicit_shortest_path_dijkstra_distance_graph
 
 
 class ApproximateShortestPathTreeDistanceGraph(ApproximateShortestPathTree):
-    pass
+
+    def __init__(self, source, graph, epsilon, sources=None):
+        seeds, source = self._get_sources(graph, source, sources)
+
+        self._source = source
+        self._graph = DistanceGraph(graph)
+        self._epsilon = epsilon
+        # memoising this because its value gets used _heavily_ in lower bound calcs, called during heuristic generation
+        self._divisor = 1 / (1 + epsilon)
+        self._sources = sources
+
+        distances = np.ones(len(graph)) * float('+inf')
+        for seed in seeds:
+            distances[seed] = 0
+
+        self._distances = implicit_shortest_path_dijkstra_distance_graph(self._graph, self._source, distances, seeds=seeds)
+
+    def update_edges(self, edges):
+        dropnodes = set()
+        for item in edges:
+            left = item[0]
+            right = item[1]
+            leftdist = self._distances[left]
+            rightdist = self._distances[right]
+            shelf = self._graph._arcs[left]
+            matches = [dist_head for (head, dist_head) in shelf if head == right]
+            weight = matches[0]
+            delta = abs(leftdist - rightdist)
+
+            if delta >= (1 + self._epsilon) * weight:
+                dropnodes.add(left)
+                dropnodes.add(right)
+
+        # if no nodes are to be dropped, nothing to do - bail out
+        if 0 == len(dropnodes):
+            return
+
+        self._distances = implicit_shortest_path_dijkstra_distance_graph(self._graph, self._source,
+                                                                         distance_labels=self._distances,
+                                                                         seeds=dropnodes)
+
+    def lighten_edge(self, u, v, weight):
+        self._graph.lighten_edge(u, v, weight)
