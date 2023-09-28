8783d41e1625ef6b5524e5359aa84d22684f4d0c
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-09-29 02:46:36 +1000

    Tickle delta debugging command

--- Tests/testDeltaDebug.py
+++ Tests/testDeltaDebug.py
@@ -1,21 +1,21 @@
-import subprocess
-import sys
+import os
 import unittest
+
 from Tests.baseTest import baseTest
+from pytest_console_scripts import ScriptRunner
 
 
 class testDeltaDebug(baseTest):
+
     def test_delta_debug_command_empty_args(self):
         fullpath = self.unpack_filename('../PyRoute/DeltaDebug/DeltaDebug.py')
 
-        args = [
-            sys.executable,
-            fullpath
-        ]
+        cwd = os.getcwd()
+        runner = ScriptRunner(launch_mode="inprocess", rootdir=cwd)
 
-        foo = subprocess.run(args=args, capture_output=True, text=True)
+        foo = runner.run(command=fullpath)
 
-        self.assertEqual(0, foo.returncode, "DeltaDebug did not complete successfully")
+        self.assertEqual(0, foo.returncode, "DeltaDebug did not complete successfully: " + foo.stderr)
         expected = '0 sectors read\nReducing by sector\nReducing by subsector\nReducing by line\n'
         actual = foo.stderr
         self.assertEqual(expected, actual)
