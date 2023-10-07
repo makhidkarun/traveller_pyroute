59e70619e9686234aaa2df703b9e9d9e4e85298b
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-10-07 23:37:36 +1000

    Characterse route-link reduction

--- Tests/testDeltaStarReduction.py
+++ Tests/testDeltaStarReduction.py
@@ -21,5 +21,21 @@ class testDeltaStarReduction(baseTest):
 
         self.assertEqual(exp, actual)
 
+    def test_reduce_routes_on_none(self):
+        src = "0216 Ambemshan            A5457BC-B Ag Cp Pi Pz (Cassilldans) { 4 }  (D6E+5) [AB8E] BCDF NS A 913 10 ImDa M3 V M6 V"
+
+        exp = "0216 Ambemshan            A5457BC-B (Cassilldans) Ag Cp Pi Pz { 4 }  (D6E+5) [AB8E] BCDF NS A 913 10 ImDa M3 V M6 V                                               "
+        actual = DeltaStar.reduce(src, drop_routes=True)
+
+        self.assertEqual(exp, actual)
+
+    def test_reduce_routes_on_existing(self):
+        src = "0410 Omwyf                B75978A-B                           { 2 }  (D6C+4) [997D] B    N  - 604 9  ImDa G1 V           Xb:0213 Xb:0609"
+
+        exp = "0410 Omwyf                B75978A-B                           { 2 }  (D6C+4) [997D] B    N  - 604 9  ImDa G1 V                                                    "
+        actual = DeltaStar.reduce(src, drop_routes=True)
+
+        self.assertEqual(exp, actual)
+
 if __name__ == '__main__':
     unittest.main()
