962b36e2a23e917d35be7436bae4f01e7876f8da
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-10-18 15:46:08 +1000

    Re-arrange pass order in deltadebug parms

--- PyRoute/DeltaDebug/DeltaDebug.py
+++ PyRoute/DeltaDebug/DeltaDebug.py
@@ -93,14 +93,14 @@ def process():
                        help="Line required to classify run output as interesting")
     delta.add_argument('--interesting-type', dest="interestingtype", default=None,
                        help="Exception type required to classify run output as interesting")
-    delta.add_argument('--two-minimise', dest="two_min", default=False, action='store_true',
-                       help="Try all pairs of star lines to see if any can be removed")
     delta.add_argument('--no-sector', dest="run_sector", default=True, action='store_false',
                        help="Skip sector-level reduction")
     delta.add_argument('--no-subsector', dest="run_subsector", default=True, action='store_false',
                        help="Skip subsector-level reduction")
     delta.add_argument('--no-line', dest="run_line", default=True, action='store_false',
-                       help="Skip line-level reduction.  At least one of sector, subsector, line and two-minimisation must be selected")
+                       help="Skip line-level reduction.")
+    delta.add_argument('--two-minimise', dest="two_min", default=False, action='store_true',
+                       help="Try all pairs of star lines to see if any can be removed.  At least one of sector, subsector, line and two-minimisation must be selected")
     delta.add_argument('--within-line', dest="run_within", default=False, action='store_true',
                        help="Try to remove irrelevant components (eg base codes) from _within_ individual lines")
     delta.add_argument('--allegiance', dest="run_allegiance", default=False, action='store_true',
