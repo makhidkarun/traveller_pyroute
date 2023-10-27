dfda7d67a3bc5a77d7fd86a493fd6ac91c4ae73d
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-10-27 21:45:10 +1000

    Flesh out invalid/missing code handling

--- Tests/Hypothesis/testDeltaStar.py
+++ Tests/Hypothesis/testDeltaStar.py
@@ -22,7 +22,7 @@ def trade_code(draw):
 
     strat = sampled_from(tradecodes)
 
-    return draw(lists(strat, min_size=1, max_size=12))
+    return draw(lists(strat, min_size=2, max_size=12))
 
 @composite
 def starline(draw):
@@ -130,8 +130,11 @@ class testDeltaStar(unittest.TestCase):
                 outer_logger.debug('Dummy message to keep assertLogs happy')
                 inner_logger.debug('Dummy message to keep assertLogs happy')
                 star1 = DeltaStar.parse_line_into_star(starline, sector, 'fixed', 'fixed')
+                star1.index = 0
+                star1.allegiance_base = 'NaHu'
                 self.assertIsNotNone(star1, "Fatal failure parsing line: " + starline)
                 self.assertTrue(isinstance(star1, DeltaStar), "Error parsing line: " + starline)
+
                 assume(0 < len(inner_logs.output))
 
                 output = outer_logs.output
@@ -152,6 +155,90 @@ class testDeltaStar(unittest.TestCase):
                     num_output,
                     "Mismatch between parsing logs and canonical-check: " + starline + '\n' + tail
                 )
+
+    @given(starline())
+    @settings(suppress_health_check=[HealthCheck(3), HealthCheck(2)])
+    @example('0101 0                    A000000-0 As De                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Ga                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Fl                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As He                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A001000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Ic                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Oc                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Po                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A010000-0 As Va                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Wa                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Na                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Ag                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Pa                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Pi                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Pr                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As In                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Ri                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Lo                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Ph                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Ni                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 As Hi                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    def test_canonicalise_invalid_trade_codes(self, starline):
+        sector = Sector('# Core', '# 0, 0')
+        star1 = DeltaStar.parse_line_into_star(starline, sector, 'fixed', 'fixed')
+        star1.index = 0
+        star1.allegiance_base = 'NaHu'
+
+        canonical_result, canonical_messages = star1.check_canonical()
+
+        star1.canonicalise()
+
+        nu_result, nu_messages = star1.check_canonical()
+        invalid = [item for item in nu_messages if 'Found invalid' in item and ('trade codes' in item or 'code on world' in item)]
+
+        self.assertTrue(
+            len(canonical_messages) >= len(nu_messages),
+            'New canonical-check messages should not happen: \n' + starline
+        )
+        self.assertEqual(0, len(invalid), 'At least one invalid trade code remaining: \n' + starline)
+
+    @given(starline())
+    @settings(suppress_health_check=[HealthCheck(3), HealthCheck(2)])
+    @example('0101 0                    A000000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000100-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000000-0 De De                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000400-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A001000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A020000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000600-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000700-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A0A1000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000800-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A044400-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000900-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A000900-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A044500-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A320000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A33A000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A060500-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    AA3A000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A060600-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    @example('0101 0                    A655000-0 As As                                  { 0 } (000+0) [0000] - - A 000 0 NaHu G5 V')
+    def test_canonicalise_missing_trade_codes(self, starline):
+        sector = Sector('# Core', '# 0, 0')
+        star1 = DeltaStar.parse_line_into_star(starline, sector, 'fixed', 'fixed')
+        star1.index = 0
+        star1.allegiance_base = 'NaHu'
+
+        canonical_result, canonical_messages = star1.check_canonical()
+
+        star1.canonicalise()
+
+        nu_result, nu_messages = star1.check_canonical()
+        invalid = [item for item in nu_messages if 'not in trade codes' in item]
+
+        self.assertTrue(
+            len(canonical_messages) >= len(nu_messages),
+            'New canonical-check messages should not happen: \n' + starline
+        )
+        badline = '' if 0 == len(invalid) else invalid[0]
+        self.assertEqual(0, len(invalid), 'At least one missing trade code not added: \n' + starline + '\n' + badline)
 
 
 if __name__ == '__main__':
