a8b8e7d96823a89cf8601810fe1cdeb5abb372ae
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-11-04 22:36:18 +1000

    Verify hex_to_axial round trips

--- Tests/Hypothesis/Position/testHexGubbins.py
+++ Tests/Hypothesis/Position/testHexGubbins.py
@@ -40,6 +40,24 @@ class testHexGubbins(unittest.TestCase):
 
         self.assertEqual(start_pos, restart_pos, "Restart should be same hex as starting position")
 
+    @given(integers(min_value=-200, max_value=200), integers(min_value=-200, max_value=200))
+    @example(1, 0)
+    @example(1, 1)
+    @example(1, -1)
+    @example(0, 0)
+    @example(0, 1)
+    @example(0, -1)
+    @example(-1, 0)
+    @example(-1, 1)
+    @example(-1, -1)
+    def test_hex_to_axial_round_trip(self, row, col):
+        q, r = Hex.hex_to_axial(row, col)
+
+        nu_row, nu_col = Hex.axial_to_hex(q, r)
+        inputs = str((row, col))
+        self.assertEqual(row, nu_row, "Row co-ordinate not round-tripped.  Hypothesis input: " + inputs)
+        self.assertEqual(col, nu_col, "Column co-ordinate not round-tripped.  Hypothesis input: " + inputs)
+
     def choose_sector(self, sector_choice):
         return self.sectors[sector_choice]
 
