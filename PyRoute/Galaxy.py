8f0a20559fd7ac8024af017887ba0d376dc91732
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-11-11 01:02:06 +1000

    Fix hypothesis-test blowups

--- PyRoute/Galaxy.py
+++ PyRoute/Galaxy.py
@@ -82,31 +82,71 @@ class Allegiance(AreaItem):
 
     @staticmethod
     def allegiance_name(name, code, base):
+        if not isinstance(name, str):
+            raise ValueError("Name must be string - received " + str(name))
+        if not isinstance(code, str):
+            raise ValueError("Code must be string - received " + str(code))
+        if 4 < len(code):
+            raise ValueError("Code must not exceed 4 characters - received " + str(code))
+        name = name.strip()
+        if '' == name:
+            raise ValueError("Name must not be empty string")
+        if code.startswith('As'):
+            if 5 < name.count(','):
+                raise ValueError("Name must have at most five commas")
+        else:
+            if 1 < name.count(','):
+                raise ValueError("Name must have at most one comma")
+        namelen = 0
+        while namelen != len(name):
+            namelen = len(name)
+            name = name.replace('  ', ' ')
         if base:
             return name
         names = name.split(',') if ',' in name else [name, '']
+        names[0] = names[0].strip()
+        names[1] = names[1].strip()
         if code.startswith('Na'):
-            return '{} {}'.format(names[0], names[1].strip())
+            return '{} {}'.format(names[0], names[1])
         elif code.startswith('Cs'):
-            return '{}s of the {}'.format(names[0].strip(), names[1].strip())
+            return '{}s of the {}'.format(names[0], names[1])
         elif ',' in name:
-            return '{}, {}'.format(names[0].strip(), names[1].strip())
+            return '{}, {}'.format(names[0], names[1])
         return '{}'.format(name.strip())
 
 
     @staticmethod
     def set_wiki_name(name, code, base):
+        if not isinstance(name, str):
+            raise ValueError("Name must be string - received " + str(name))
+        if not isinstance(code, str):
+            raise ValueError("Code must be string - received " + str(code))
+        name = name.strip()
+        if '' == name:
+            raise ValueError("Name must not be empty string")
+        if ',' == name:
+            raise ValueError("Name must not be pair of empty strings")
+        if '[' in name or ']' in name:
+            raise ValueError("Name must not contain square brackets")
+
         names = name.split(',') if ',' in name else [name, '']
+        names[0] = names[0].strip()
+        names[1] = names[1].strip()
+        if '' == names[0]:
+            raise ValueError("First part of name string must not be an empty string itself")
+        if '' == names[1] and ',' in name:
+            raise ValueError("Second part of name string must not be an empty string itself")
+
         if code.startswith('Na'):
-            return '[[{}]] {}'.format(names[0].strip(), names[1].strip())
+            return '[[{}]] {}'.format(names[0], names[1])
         elif code.startswith('Cs'):
-            return '[[{}]]s of the [[{}]]'.format(names[0].strip(), names[1].strip())
+            return '[[{}]]s of the [[{}]]'.format(names[0], names[1])
         elif ',' in name:
             if base:
-                return '[[{}]]'.format(names[0].strip())
+                return '[[{}]]'.format(names[0])
             else:
-                return '[[{}]], [[{}]]'.format(names[0].strip(), names[1].strip())
-        return '[[{}]]'.format(name.strip())
+                return '[[{}]], [[{}]]'.format(names[0], names[1])
+        return '[[{}]]'.format(name)
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.code)
