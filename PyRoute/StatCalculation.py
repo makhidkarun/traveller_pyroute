bea74bc8023c43705832275b38e5b87c7ca4c0b3
Alex Goodwin <cyberia.resurrection@gmail.com>
2023-11-30 03:29:46 +1000

    Handle nonstellar primaries in star counting

--- PyRoute/StatCalculation.py
+++ PyRoute/StatCalculation.py
@@ -300,7 +300,7 @@ class StatCalculation(object):
             star_list = star.star_list
             stats.stars += len(star_list)
             stats.star_count[len(star_list)] += 1
-            primary_type = star_list[0].spectral
+            primary_type = star_list[0].spectral if star_list[0].spectral is not None else star_list[0].size
             stats.primary_count[primary_type] += 1
 
         for code in star.baseCode:
