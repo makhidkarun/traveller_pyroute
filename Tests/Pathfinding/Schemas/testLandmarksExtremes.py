ec0e717fc93db39bae12d1897b038a03616fccfa
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-02-19 15:07:25 +1000

    Land initial tri-axis extreme landmark schema

--- Tests/Pathfinding/Schemas/testLandmarksExtremes.py
+++ Tests/Pathfinding/Schemas/testLandmarksExtremes.py
@@ -8,6 +8,7 @@ from PyRoute.DeltaDebug.DeltaDictionary import SectorDictionary, DeltaDictionary
 from PyRoute.Pathfinding.LandmarkSchemes.LandmarksQExtremes import LandmarksQExtremes
 from PyRoute.Pathfinding.LandmarkSchemes.LandmarksRExtremes import LandmarksRExtremes
 from PyRoute.Pathfinding.LandmarkSchemes.LandmarksSExtremes import LandmarksSExtremes
+from PyRoute.Pathfinding.LandmarkSchemes.LandmarksTriaxialExtremes import LandmarksTriaxialExtremes
 from Tests.baseTest import baseTest
 
 
@@ -88,5 +89,32 @@ class testLandmarksExtremes(baseTest):
                 {0: 413, 1: 165, 2: 293, 3: 368, 4: 408, 5: 409, 6: 414, 7: 415},
                 {0: 126, 1: 165, 2: 293, 3: 368, 4: 408, 5: 411, 6: 416, 7: 415}
             ]
+        actual = foo.get_landmarks(index=True)
+        self.assertEqual(expected, actual)
+
+    def test_axial_extremes_of_zarushagar(self):
+        sourcefile = self.unpack_filename('../DeltaFiles/Zarushagar.sec')
+
+        sector = SectorDictionary.load_traveller_map_file(sourcefile)
+        delta = DeltaDictionary()
+        delta[sector.name] = sector
+
+        args = self._make_args()
+
+        galaxy = DeltaGalaxy(args.btn, args.max_jump)
+        galaxy.read_sectors(delta, args.pop_code, args.ru_calc,
+                            args.route_reuse, args.routes, args.route_btn, args.mp_threads, args.debug_flag)
+        galaxy.output_path = args.output
+
+        galaxy.generate_routes()
+        galaxy.trade.calculate_components()
+
+        foo = LandmarksTriaxialExtremes(galaxy)
+
+        expected = [
+            {0: 132, 1: 165, 2: 293, 3: 368, 4: 408, 5: 411, 6: 420, 7: 415},
+            {0: 486, 1: 165, 2: 293, 3: 368, 4: 408, 5: 409, 6: 420, 7: 415},
+            {0: 126, 1: 165, 2: 293, 3: 368, 4: 408, 5: 411, 6: 416, 7: 415}
+        ]
         actual = foo.get_landmarks(index=True)
         self.assertEqual(expected, actual)
