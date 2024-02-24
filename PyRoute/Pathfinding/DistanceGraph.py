35c0fc68c293f52138a813d15a66305451b55afe
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-02-25 02:02:19 +1000

    Track min-edge-cost vector in distanceGraph

--- PyRoute/Pathfinding/DistanceGraph.py
+++ PyRoute/Pathfinding/DistanceGraph.py
@@ -17,10 +17,17 @@ class DistanceGraph:
             (np.array(graph.adj[u]), np.array([data['weight'] for data in list(graph.adj[u].values())], dtype=float))
             for u in self._nodes
         ]
+        self._min_cost = np.zeros(len(self._nodes))
+        for i in range(0, len(self._nodes)):
+            node_edges = self._arcs[i][1]
+            if 0 < len(node_edges):
+                self._min_cost[i] = min(node_edges)
 
     def lighten_edge(self, u, v, weight):
         self._lighten_arc(u, v, weight)
         self._lighten_arc(v, u, weight)
+        self._min_cost[u] = min(self._min_cost[u], weight)
+        self._min_cost[v] = min(self._min_cost[v], weight)
 
     def _lighten_arc(self, u, v, weight):
         arcs = self._arcs[u]
