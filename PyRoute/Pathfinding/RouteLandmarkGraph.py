ff623889ce03af8678e3675f077b95d6296aee66
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-02-26 01:23:46 +1000

    Start fleshing out route landmark graph

---
+++ PyRoute/Pathfinding/RouteLandmarkGraph.py
@@ -0,0 +1,44 @@
+"""
+Created on Feb 25, 2024
+
+@author: CyberiaResurrection
+"""
+import numpy as np
+
+from PyRoute.Pathfinding.DistanceBase import DistanceBase
+
+
+class RouteLandmarkGraph(DistanceBase):
+
+    def __init__(self, graph):
+        super().__init__(graph)
+        self._arcs = [
+            (np.array([], dtype=int), np.array([], dtype=float))
+            for u in self._nodes
+        ]
+
+    def __getitem__(self, item):
+        self._check_index(item)
+        return self._arcs[item]
+
+    def add_edge(self, u, v, weight):
+        self._check_index(u)
+        self._check_index(v)
+        self._extend_arc(u, v, weight)
+        self._extend_arc(v, u, weight)
+
+    def _extend_arc(self, u, v, weight):
+        u_first = self._arcs[u][0]
+        u_last = self._arcs[u][0]
+        u_first = np.append(u_first, [v])
+        u_last = np.append(u_last, [weight])
+        self._arcs[u] = (u_first, u_last)
+
+    def _check_index(self, item):
+        if not isinstance(item, int):
+            raise IndexError("Index must be integer between 0 and " + str(len(self) - 1))
+        if 0 > item:
+            raise IndexError("Index must be integer between 0 and " + str(len(self) - 1))
+        if len(self) <= item:
+            raise IndexError("Index must be integer between 0 and " + str(len(self) - 1))
+
