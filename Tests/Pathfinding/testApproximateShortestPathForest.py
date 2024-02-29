e380a5e0d784a942950389389f15d4e1b56b03c2
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-03-01 02:11:32 +1000

    Spin up unified approx-SP forest

--- Tests/Pathfinding/testApproximateShortestPathForest.py
+++ Tests/Pathfinding/testApproximateShortestPathForest.py
@@ -18,6 +18,7 @@ from PyRoute.Pathfinding.LandmarkSchemes.LandmarksWTNExtremes import LandmarksWT
 from PyRoute.DeltaDebug.DeltaDictionary import SectorDictionary, DeltaDictionary
 from PyRoute.DeltaDebug.DeltaGalaxy import DeltaGalaxy
 from PyRoute.Pathfinding.ApproximateShortestPathForestDistanceGraph import ApproximateShortestPathForestDistanceGraph
+from PyRoute.Pathfinding.ApproximateShortestPathForestUnified import ApproximateShortestPathForestUnified
 from Tests.baseTest import baseTest
 
 
@@ -69,6 +70,101 @@ class testApproximateShortestPathForest(baseTest):
         self.assertIsNotNone(actual)
 
         np.testing.assert_array_almost_equal(expected, actual, 0.000001, "Unexpected bounds array")
+
+    def test_trixial_bounds_in_bulk_unified(self):
+        galaxy = self.set_up_zarushagar_sector()
+
+        foo = LandmarksTriaxialExtremes(galaxy)
+        landmarks = foo.get_landmarks(index=True)
+        graph = galaxy.stars
+        stars = list(graph.nodes)
+        source = stars[0]
+
+        approx = ApproximateShortestPathForestUnified(source, graph, 0.2, sources=landmarks)
+        self.assertEqual(3, approx._num_trees)
+
+        active_nodes = [2, 80]
+        target = 80
+        expected = np.array([310.833, 0])
+        actual = approx.lower_bound_bulk(active_nodes, target)
+        self.assertIsNotNone(actual)
+
+        np.testing.assert_array_almost_equal(expected, actual, 0.000001, "Unexpected bounds array")
+
+    def test_unified_can_handle_singleton_landmarks(self):
+        galaxy = self.set_up_zarushagar_sector()
+
+        foo = LandmarksTriaxialExtremes(galaxy)
+        landmarks = foo.get_landmarks(index=True)[0]
+        graph = galaxy.stars
+        stars = list(graph.nodes)
+        source = stars[0]
+
+        approx = ApproximateShortestPathForestUnified(source, graph, 0.2, sources=landmarks)
+        self.assertEqual(1, approx._num_trees)
+
+    def test_verify_near_root_edge_propagates(self):
+        sourcefile = self.unpack_filename('DeltaFiles/Zarushagar-Ibara.sec')
+        jsonfile = self.unpack_filename('PathfindingFiles/single_source_distances_ibara_subsector_from_0101.json')
+
+        sector = SectorDictionary.load_traveller_map_file(sourcefile)
+        delta = DeltaDictionary()
+        delta[sector.name] = sector
+
+        args = self._make_args()
+
+        galaxy = DeltaGalaxy(args.btn, args.max_jump)
+        galaxy.read_sectors(delta, args.pop_code, args.ru_calc,
+                            args.route_reuse, args.routes, args.route_btn, 1, False)
+        galaxy.output_path = args.output
+
+        galaxy.generate_routes()
+        galaxy.trade.calculate_components()
+
+        graph = galaxy.stars
+        stars = list(graph.nodes)
+        source = stars[0]
+        leafnode = stars[30]
+
+        approx = ApproximateShortestPathForestUnified(source, graph, 0)
+
+        # auxiliary network dijkstra calculation to dish out parent nodes
+        paths = nx.single_source_dijkstra_path(graph, source)
+        right = paths[leafnode][1]
+
+        # seed expected distances
+        with open(jsonfile, 'r') as file:
+            expected_string = json.load(file)
+
+        expected_distances = dict()
+        component = [item for item in stars if graph.nodes[item]['star'].component == graph.nodes[source]['star'].component]
+        for item in component:
+            exp_dist = 0
+            rawstar = graph.nodes[item]['star']
+            if str(rawstar) in expected_string:
+                exp_dist = expected_string[str(rawstar)]
+            expected_distances[item] = exp_dist
+
+        distance_check = list(expected_distances.values()) == approx._distances[:,0]
+        self.assertTrue(distance_check.all(), "Unexpected distances after SPT creation")
+
+        # adjust weight
+        oldweight = galaxy.stars[source][right]['weight']
+        galaxy.stars[source][right]['weight'] -= 1
+        galaxy.trade.star_graph.lighten_edge(source, right, oldweight - 1)
+        approx._graph.lighten_edge(source, right, oldweight - 1)
+
+        # tell SPT weight has changed
+        edge = (source, right)
+
+        for item in expected_distances:
+            if expected_distances[item] > 0 and 'Selsinia (Zarushagar 0201)' != str(graph.nodes[item]['star']):
+                expected_distances[item] -= 1
+
+        approx.update_edges([edge])
+
+        distance_check = list(expected_distances.values()) == approx._distances[:, 0]
+        self.assertTrue(distance_check.all(), "Unexpected distances after SPT restart")
 
     def set_up_zarushagar_sector(self):
         sourcefile = self.unpack_filename('DeltaFiles/Zarushagar.sec')
