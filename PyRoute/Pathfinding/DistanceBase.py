493cecf95ad8484b4a96c2636793ec2f1ace8f5f
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-03-04 16:31:16 +1000

    Hook up bulk node distance retrieval

--- PyRoute/Pathfinding/DistanceBase.py
+++ PyRoute/Pathfinding/DistanceBase.py
@@ -3,19 +3,34 @@ Created on Feb 25, 2024
 
 @author: CyberiaResurrection
 """
+import numpy as np
 
 
 class DistanceBase:
 
     def __init__(self, graph):
-        self._nodes = list(graph.nodes())
+        raw_nodes = graph.nodes()
+        self._nodes = list(raw_nodes)
+        num_nodes = len(self._nodes)
         self._indexes = {node: i for (i, node) in enumerate(self._nodes)}
+        positions = [
+            raw_nodes[u]['star'].hex.hex_position() for u in self._nodes
+        ]
+        self._positions = np.zeros((num_nodes, 2), dtype=int)
+        for i in range(num_nodes):
+            self._positions[i, :] = np.array(positions[i])
 
     def __len__(self):
         return len(self._nodes)
 
     def lighten_edge(self, u, v, weight):
         raise NotImplementedError("Base Class")
+
+    def distances_from_target(self, target):
+        dq = self._positions[:, 0] - self._positions[target, 0]
+        dr = self._positions[:, 1] - self._positions[target, 1]
+
+        return (abs(dq) + abs(dr) + abs(dq + dr)) // 2
 
     def _lighten_arc(self, u, v, weight):
         arcs = self._arcs[u]
