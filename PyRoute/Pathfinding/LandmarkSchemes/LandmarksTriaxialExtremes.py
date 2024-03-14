f239ddf933861565e6d568bee6071e0a9f8ce752
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-03-14 12:14:19 +1000

    Lay size-adaption groundwork for triaxial landmarks

--- PyRoute/Pathfinding/LandmarkSchemes/LandmarksTriaxialExtremes.py
+++ PyRoute/Pathfinding/LandmarkSchemes/LandmarksTriaxialExtremes.py
@@ -3,21 +3,29 @@ Created on Feb 19, 2024
 
 @author: CyberiaResurrection
 """
+import math
 
 
 class LandmarksTriaxialExtremes:
 
     def __init__(self, galaxy):
         self.galaxy = galaxy
+        self.max_slots = 3
 
     def get_landmarks(self, index=False):
+        max_size = max(self.galaxy.trade.components.values())
+        num_slots = min(self.max_slots, 3 * math.ceil(math.log10(max_size)))
         result = []
 
-        result.append(dict())
-        result.append(dict())
-        result.append(dict())
+        for i in range(num_slots):
+            result.append(dict())
+
         for component_id in self.galaxy.trade.components:
             comp_size = self.galaxy.trade.components[component_id]
+            # No point generating landmarks for a singleton component, as it will never be used in pathfinding
+            if 2 > comp_size:
+                continue
+            # slots = min(self.max_slots, 3 * math.ceil(math.log10(comp_size)))
 
             stars = [item for item in self.galaxy.star_mapping.values() if component_id == item.component]
             # maximum q in component
@@ -26,9 +34,6 @@ class LandmarksTriaxialExtremes:
                 result[0][component_id] = source.index
             else:
                 result[0][component_id] = source
-
-            if 2 > comp_size:
-                continue
 
             # minimum r in component
             source = min(stars, key=lambda item: item.hex.r)
