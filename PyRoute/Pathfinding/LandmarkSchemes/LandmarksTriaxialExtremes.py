bd724fb97a455277cf365e262593fa6646550638
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-03-14 13:09:33 +1000

    Add other sides of triaxial extremes

--- PyRoute/Pathfinding/LandmarkSchemes/LandmarksTriaxialExtremes.py
+++ PyRoute/Pathfinding/LandmarkSchemes/LandmarksTriaxialExtremes.py
@@ -10,7 +10,7 @@ class LandmarksTriaxialExtremes:
 
     def __init__(self, galaxy):
         self.galaxy = galaxy
-        self.max_slots = 3
+        self.max_slots = 6
 
     def get_landmarks(self, index=False):
         max_size = max(self.galaxy.trade.components.values())
@@ -25,7 +25,7 @@ class LandmarksTriaxialExtremes:
             # No point generating landmarks for a singleton component, as it will never be used in pathfinding
             if 2 > comp_size:
                 continue
-            # slots = min(self.max_slots, 3 * math.ceil(math.log10(comp_size)))
+            slots = min(self.max_slots, 3 * math.ceil(math.log10(comp_size)))
 
             stars = [item for item in self.galaxy.star_mapping.values() if component_id == item.component]
             stars.sort(key=lambda item: item.wtn, reverse=True)
@@ -36,6 +36,9 @@ class LandmarksTriaxialExtremes:
             else:
                 result[0][component_id] = source
 
+            if 1 == slots:
+                continue
+
             # minimum r in component
             source = min(stars, key=lambda item: item.hex.r)
             if index:
@@ -43,11 +46,47 @@ class LandmarksTriaxialExtremes:
             else:
                 result[1][component_id] = source
 
+            if 2 == slots:
+                continue
+
             # minimum s in component
             source = min(stars, key=lambda item: -item.hex.q - item.hex.r)
             if index:
                 result[2][component_id] = source.index
             else:
                 result[2][component_id] = source
+
+            if 3 == slots:
+                continue
+
+            # minimum q in component
+            source = min(stars, key=lambda item: item.hex.q)
+            if index:
+                result[3][component_id] = source.index
+            else:
+                result[3][component_id] = source
+
+            if 4 == slots:
+                continue
+
+            # maximum r in component
+            source = max(stars, key=lambda item: item.hex.r)
+            if index:
+                result[4][component_id] = source.index
+            else:
+                result[4][component_id] = source
+
+            if 5 == slots:
+                continue
+
+            # maximum s in component
+            source = max(stars, key=lambda item: -item.hex.q - item.hex.r)
+            if index:
+                result[5][component_id] = source.index
+            else:
+                result[5][component_id] = source
+
+            if 6 == slots:
+                continue
 
         return result
