42d895f77203d43c5f842a63a5e72200925dd0a9
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-05-29 01:01:21 +1000

    Hook canonicalisation to delta debugging

--- PyRoute/DeltaStar.py
+++ PyRoute/DeltaStar.py
@@ -13,12 +13,14 @@ from PyRoute.TradeCodes import TradeCodes
 class DeltaStar(Star):
 
     @staticmethod
-    def reduce(starline, drop_routes=False, drop_trade_codes=False, drop_noble_codes=False, drop_base_codes=False, drop_trade_zone=False, drop_extra_stars=False, reset_pbg=False, reset_worlds=False, reset_port=False, reset_tl=False, reset_sophont=False, reset_capitals=False):
+    def reduce(starline, drop_routes=False, drop_trade_codes=False, drop_noble_codes=False, drop_base_codes=False, drop_trade_zone=False, drop_extra_stars=False, reset_pbg=False, reset_worlds=False, reset_port=False, reset_tl=False, reset_sophont=False, reset_capitals=False, canonicalise=False):
         sector = Sector("# dummy", "# 0, 0")
         star = DeltaStar.parse_line_into_star(starline, sector, 'fixed', 'fixed')
         if not isinstance(star, DeltaStar):
             return None
 
+        if canonicalise:
+            star.canonicalise()
         if drop_routes:
             star.reduce_routes()
         if drop_trade_codes:
@@ -49,7 +51,7 @@ class DeltaStar(Star):
 
     @staticmethod
     def reduce_all(original):
-        return DeltaStar.reduce(original, drop_routes=True, drop_trade_codes=True, drop_noble_codes=True, drop_base_codes=True, drop_trade_zone=True, drop_extra_stars=True, reset_pbg=True, reset_worlds=True, reset_port=True, reset_tl=True, reset_sophont=True)
+        return DeltaStar.reduce(original, drop_routes=True, drop_trade_codes=True, drop_noble_codes=True, drop_base_codes=True, drop_trade_zone=True, drop_extra_stars=True, reset_pbg=True, reset_worlds=True, reset_port=True, reset_tl=True, reset_sophont=True, canonicalise=True)
 
     @staticmethod
     def reduce_auxiliary(original):
