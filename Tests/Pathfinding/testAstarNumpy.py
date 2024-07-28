0bc93328679150ab3b755510ca584f0d6370e507
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-07-28 23:24:24 +1000

    Fix test blast damage

--- Tests/Pathfinding/testAstarNumpy.py
+++ Tests/Pathfinding/testAstarNumpy.py
@@ -39,9 +39,9 @@ class testAStarNumpy(baseTest):
         heuristic = galaxy.heuristic_distance_bulk
 
         exp_route = [0, 8, 9, 15, 24, 36]
-        exp_diagnostics = {'branch_factor': 1.704, 'f_exhausted': 3, 'g_exhausted': 3, 'neighbour_bound': 14,
-                            'new_upbounds': 1, 'nodes_expanded': 16, 'nodes_queued': 18, 'nodes_revisited': 1,
-                            'num_jumps': 5, 'un_exhausted': 7, 'targ_exhausted': 1}
+        exp_diagnostics = {'branch_factor': 1.789, 'f_exhausted': 0, 'g_exhausted': 4, 'neighbour_bound': 15,
+                            'new_upbounds': 1, 'nodes_expanded': 17, 'nodes_queued': 21, 'nodes_revisited': 1,
+                            'num_jumps': 5, 'un_exhausted': 10, 'targ_exhausted': 1}
 
         upbound = galaxy.trade.shortest_path_tree.triangle_upbound(source, target) * 1.005
         act_route, diagnostics = astar_path_numpy(dist_graph, source.index, target.index, heuristic, upbound=upbound,
