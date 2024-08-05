e8a84b8e9b882baf7db490714cd7be1012cd2671
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-08-05 21:45:58 +1000

    Snake-case var names in SectorHexMap

--- PyRoute/Outputs/SectorHexMap.py
+++ PyRoute/Outputs/SectorHexMap.py
@@ -19,31 +19,31 @@ class SectorHexMap(Map):
     def comm_line(self, pdf, edge):
         raise NotImplementedError("Base Class")
 
-    def clipping(self, startx, starty, endx, endy):
+    def clipping(self, start_x, start_y, end_x, end_y):
         points_t = [0.0, 1.0]
-        line_pt_1 = [startx, starty]
-        line_pt_2 = [endx, endy]
+        line_pt_1 = [start_x, start_y]
+        line_pt_2 = [end_x, end_y]
 
-        if startx == endx:
-            if starty > endy:
-                return ((startx, min(max(starty, endy), 780)),
-                        (startx, max(min(starty, endy), 42)))
+        if start_x == end_x:
+            if start_y > end_y:
+                return ((start_x, min(max(start_y, end_y), 780)),
+                        (start_x, max(min(start_y, end_y), 42)))
             else:
-                return ((startx, max(min(starty, endy), 42)),
-                        (startx, min(max(starty, endy), 780)))
+                return ((start_x, max(min(start_y, end_y), 42)),
+                        (start_x, min(max(start_y, end_y), 780)))
 
-        if starty == endy:
-            if startx > endx:
-                return ((min(max(startx, endx), 600), starty),
-                        (max(min(startx, endx), 15), starty))
+        if start_y == end_y:
+            if start_x > end_x:
+                return ((min(max(start_x, end_x), 600), start_y),
+                        (max(min(start_x, end_x), 15), start_y))
             else:
-                return ((max(min(startx, endx), 15), starty),
-                        (min(max(startx, endx), 600), starty))
+                return ((max(min(start_x, end_x), 15), start_y),
+                        (min(max(start_x, end_x), 600), start_y))
 
-        points_t.append(float(15 - startx) / (endx - startx))
-        points_t.append(float(600 - startx) / (endx - startx))
-        points_t.append(float(780 - starty) / (endy - starty))
-        points_t.append(float(42 - starty) / (endy - starty))
+        points_t.append(float(15 - start_x) / (end_x - start_x))
+        points_t.append(float(600 - start_x) / (end_x - start_x))
+        points_t.append(float(780 - start_y) / (end_y - start_y))
+        points_t.append(float(42 - start_y) / (end_y - start_y))
 
         points_t.sort()
         result = [(pt_1 + t * (pt_2 - pt_1)) for t in (points_t[2], points_t[3]) for (pt_1, pt_2) in
@@ -62,18 +62,18 @@ class SectorHexMap(Map):
 
     def _sector_map_comm_and_trade_routes(self, comm_routes, pdf_doc, worlds):
         for (star, neighbor, data) in comm_routes:
-            srcstar = self.galaxy.star_mapping[star]
-            trgstar = self.galaxy.star_mapping[neighbor]
-            self.comm_line(pdf_doc, [srcstar, trgstar])
+            src_star = self.galaxy.star_mapping[star]
+            trg_star = self.galaxy.star_mapping[neighbor]
+            self.comm_line(pdf_doc, [src_star, trg_star])
         sector_trade = [star for star in self.galaxy.stars.edges(worlds, True)
                         if star[2]['trade'] > 0 and StatCalculation.trade_to_btn(star[2]['trade']) >= self.min_btn]
         logging.getLogger('PyRoute.HexMap').debug("Worlds with trade: {}".format(len(sector_trade)))
         sector_trade.sort(key=lambda line: line[2]['trade'])
         for (star, neighbor, data) in sector_trade:
             self.galaxy.stars[star][neighbor]['trade btn'] = StatCalculation.trade_to_btn(data['trade'])
-            srcstar = self.galaxy.star_mapping[star]
-            trgstar = self.galaxy.star_mapping[neighbor]
-            self.trade_line(pdf_doc, [srcstar, trgstar], data)
+            src_star = self.galaxy.star_mapping[star]
+            trg_star = self.galaxy.star_mapping[neighbor]
+            self.trade_line(pdf_doc, [src_star, trg_star], data)
 
     def _sector_map_systems_and_sectors(self, gal_sector, pdf_doc):
         for star in gal_sector.worlds:
@@ -120,8 +120,8 @@ class SectorHexMap(Map):
         return end, start, trade_colour
 
     def _get_line_endpoints(self, end, start):
-        starty = self.y_start + (self.ym * 2 * start.row) - (self.ym * (1 if start.col & 1 else 0))
-        startx = (self.xm * 3 * start.col) + self.ym
+        start_y = self.y_start + (self.ym * 2 * start.row) - (self.ym * (1 if start.col & 1 else 0))
+        start_x = (self.xm * 3 * start.col) + self.ym
         end_row = end.row
         end_col = end.col
         if end.sector != start.sector:
@@ -137,16 +137,16 @@ class SectorHexMap(Map):
             if end.sector.y < start.sector.y:
                 end_row += 40
                 down = True
-            endy = self.y_start + (self.ym * 2 * end_row) - (self.ym * (1 if end_col & 1 else 0))
-            endx = (self.xm * 3 * end_col) + self.ym
+            end_y = self.y_start + (self.ym * 2 * end_row) - (self.ym * (1 if end_col & 1 else 0))
+            end_x = (self.xm * 3 * end_col) + self.ym
 
-            (startx, starty), (endx, endy) = self.clipping(startx, starty, endx, endy)
+            (start_x, start_y), (end_x, end_y) = self.clipping(start_x, start_y, end_x, end_y)
             if up:
-                assert starty >= endy, "Misaligned to-coreward trade segment between " + str(start) + " and " + str(end)
+                assert start_y >= end_y, "Misaligned to-coreward trade segment between " + str(start) + " and " + str(end)
             if down:
-                assert starty <= endy, "Misaligned to-rimward trade segment between " + str(start) + " and " + str(end)
+                assert start_y <= end_y, "Misaligned to-rimward trade segment between " + str(start) + " and " + str(end)
 
         else:
-            endy = self.y_start + (self.ym * 2 * end_row) - (self.ym * (1 if end_col & 1 else 0))
-            endx = (self.xm * 3 * end_col) + self.ym
-        return endx, endy, startx, starty
+            end_y = self.y_start + (self.ym * 2 * end_row) - (self.ym * (1 if end_col & 1 else 0))
+            end_x = (self.xm * 3 * end_col) + self.ym
+        return end_x, end_y, start_x, start_y
