1383a3b67c20b84ae1b97e9b48a22605bd6b3816
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-08-19 23:08:41 +1000

    Hoist dijkstra_core out to its own file

--- PyRoute/Pathfinding/single_source_dijkstra.py
+++ PyRoute/Pathfinding/single_source_dijkstra.py
@@ -12,6 +12,8 @@ the specific input format used in PyRoute.
 import heapq
 import numpy as np
 
+from PyRoute.Pathfinding.single_source_dijkstra_core import dijkstra_core
+
 
 def implicit_shortest_path_dijkstra_distance_graph(graph, source, distance_labels, seeds=None, divisor=1, min_cost=None, max_labels=None):
     # return only distance_labels from the explicit version
@@ -40,52 +42,4 @@ def explicit_shortest_path_dijkstra_distance_graph(graph, source, distance_label
     parents = np.ones(len(graph), dtype=int) * -100  # Using -100 to track "not considered during processing"
     parents[list(seeds)] = -1  # Using -1 to flag "root node of tree"
 
-    while heap:
-        dist_tail, tail = heapq.heappop(heap)
-
-        if dist_tail > distance_labels[tail] or dist_tail + min_cost[tail] > max_neighbour_labels[tail]:
-            # Since we've just dequeued a bad node (distance exceeding its current label, or too close to max-label),
-            # remove other bad nodes from the list to avoid tripping over them later, and chuck out nodes who
-            # can't give better distance labels
-            if heap:
-                heap = [(distance, tail) for (distance, tail) in heap if distance <= distance_labels[tail]
-                        and distance + min_cost[tail] <= max_neighbour_labels[tail]]
-                heapq.heapify(heap)
-            continue
-
-        # Link weights are strictly positive, thus lower bounded by zero. Thus, when the current dist_tail value exceeds
-        # the corresponding node's distance label at the other end of the candidate edge, trim that edge.  Such edges
-        # cannot _possibly_ result in smaller distance labels.  By a similar argument, filter the remaining edges
-        # when the sum of dist_tail and that edge's weight equals or exceeds the corresponding node's distance label.
-        neighbours = arcs[tail]
-        active_nodes = neighbours[0]
-        active_costs = neighbours[1]
-        active_labels = distance_labels[active_nodes]
-        # update max label
-        max_neighbour_labels[tail] = max(active_labels)
-        # It's not worth (time wise) being cute and trying to break this up, forcing jumps in and out of numpy
-        keep = active_costs < (active_labels - dist_tail)
-        active_nodes = active_nodes[keep]
-        num_nodes = len(active_nodes)
-
-        if 0 == num_nodes:
-            continue
-        active_weights = dist_tail + divisor * active_costs[keep]
-        distance_labels[active_nodes] = active_weights
-
-        parents[active_nodes] = tail
-
-        if 1 == num_nodes:
-            heapq.heappush(heap, (active_weights[0], active_nodes[0]))
-        elif 2 == num_nodes:
-            heapq.heappush(heap, (active_weights[0], active_nodes[0]))
-            heapq.heappush(heap, (active_weights[1], active_nodes[1]))
-        elif 3 == num_nodes:
-            heapq.heappush(heap, (active_weights[0], active_nodes[0]))
-            heapq.heappush(heap, (active_weights[1], active_nodes[1]))
-            heapq.heappush(heap, (active_weights[2], active_nodes[2]))
-        else:  # Only cop the iterator overhead if there's at least 4 neighbours to queue
-            for index in range(0, num_nodes):
-                heapq.heappush(heap, (active_weights[index], active_nodes[index]))
-
-    return distance_labels, parents, max_neighbour_labels
+    return dijkstra_core(arcs, distance_labels, divisor, heap, max_neighbour_labels, min_cost, parents)
---
+++ PyRoute/Pathfinding/single_source_dijkstra_core.py
@@ -0,0 +1,56 @@
+"""
+Created on Aug 19, 2024
+
+@author: CyberiaResurrection
+"""
+import cython
+from cython.cimports.numpy import numpy as cnp
+cnp.import_array()
+from _heapq import heappop, heappush, heapify
+
+
+def dijkstra_core(arcs: cython.list[tuple[cnp.ndarray[cython.int], cnp.ndarray[cython.float]]],
+                  distance_labels: cnp.ndarray[cython.float], divisor: cython.float,
+                  heap: cython.list[tuple[cnp.ndarray[cython.float], cnp.ndarray[cython.float]]],
+                  max_neighbour_labels: cnp.ndarray[cython.float], min_cost: cnp.ndarray(cython.float),
+                  parents: cnp.ndarray[cython.int]):
+
+    while heap:
+        dist_tail, tail = heappop(heap)
+
+        if dist_tail > distance_labels[tail] or dist_tail + min_cost[tail] > max_neighbour_labels[tail]:
+            # Since we've just dequeued a bad node (distance exceeding its current label, or too close to max-label),
+            # remove other bad nodes from the list to avoid tripping over them later, and chuck out nodes who
+            # can't give better distance labels
+            if heap:
+                heap = [(distance, tail) for (distance, tail) in heap if distance <= distance_labels[tail]
+                        and distance + min_cost[tail] <= max_neighbour_labels[tail]]
+                heapify(heap)
+            continue
+
+        # Link weights are strictly positive, thus lower bounded by zero. Thus, when the current dist_tail value exceeds
+        # the corresponding node's distance label at the other end of the candidate edge, trim that edge.  Such edges
+        # cannot _possibly_ result in smaller distance labels.  By a similar argument, filter the remaining edges
+        # when the sum of dist_tail and that edge's weight equals or exceeds the corresponding node's distance label.
+        neighbours = arcs[tail]
+        active_nodes = neighbours[0]
+        active_costs = neighbours[1]
+        active_labels = distance_labels[active_nodes]
+        # update max label
+        max_neighbour_labels[tail] = max(active_labels)
+        # It's not worth (time wise) being cute and trying to break this up, forcing jumps in and out of numpy
+        keep = active_costs < (active_labels - dist_tail)
+        active_nodes = active_nodes[keep]
+        num_nodes = len(active_nodes)
+
+        if 0 == num_nodes:
+            continue
+        active_weights = dist_tail + divisor * active_costs[keep]
+        distance_labels[active_nodes] = active_weights
+
+        parents[active_nodes] = tail
+
+        for index in range(0, num_nodes):
+            heappush(heap, (active_weights[index], active_nodes[index]))
+
+    return distance_labels, parents, max_neighbour_labels
