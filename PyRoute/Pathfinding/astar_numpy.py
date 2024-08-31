09a8d6a2d9f03a0d3dce84a124ae911b8528ec00
Alex Goodwin <cyberia.resurrection@gmail.com>
2024-08-31 23:28:28 +1000

    Seed astar distances with upper bound

--- PyRoute/Pathfinding/astar_numpy.py
+++ PyRoute/Pathfinding/astar_numpy.py
@@ -82,7 +82,7 @@ def astar_path_numpy(G, source: cython.int, target: cython.int, bulk_heuristic,
     # Tracks shortest _complete_ path found so far
     upbound = np.finfo(np.float64).max if upbound is None else upbound
     # Traces lowest distance from source node found for each node
-    distances = np.ones(len_G) * float('inf')
+    distances = np.ones(len_G) * upbound
 
     # pre-calc the minimum-cost edge on each node
     min_cost = np.zeros(len_G) if min_cost is None else min_cost
