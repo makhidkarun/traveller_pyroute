64d4402cee460974ad29a57738477dbba81fbdc1
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-04-15 15:20:41 +1000

    Characterise allegiance lists on sector reads

--- Tests/AreaItems/testAllegiance.py
+++ Tests/AreaItems/testAllegiance.py
@@ -1,10 +1,18 @@
+"""
+Created on Apr 17, 2025
+
+@author: CyberiaResurrection
+"""
 import copy
-import unittest
 
 from PyRoute.AreaItems.Allegiance import Allegiance
+from PyRoute.AreaItems.Galaxy import Galaxy
+from PyRoute.DataClasses.ReadSectorOptions import ReadSectorOptions
+from PyRoute.DeltaDebug.DeltaDictionary import SectorDictionary
+from Tests.baseTest import baseTest
 
 
-class testAllegiance(unittest.TestCase):
+class testAllegiance(baseTest):
 
     def testAslanAllegianceNotBeginningWithAs(self):
         code = "A8"
@@ -25,3 +33,35 @@ class testAllegiance(unittest.TestCase):
 
         foo = copy.deepcopy(alleg)
         self.assertTrue(isinstance(foo, Allegiance))
+
+    def test_allegiance_list_on_galaxy_read(self):
+        sourcefile = self.unpack_filename('DeltaFiles/duplicate_node_blowup/Trojan Reach.sec')
+
+        args = self._make_args()
+        args.route_btn = 15
+        readparms = ReadSectorOptions(sectors=[sourcefile], pop_code=args.pop_code, ru_calc=args.ru_calc,
+                                      route_reuse=args.route_reuse, trade_choice=args.routes, route_btn=args.route_btn,
+                                      mp_threads=args.mp_threads, debug_flag=args.debug_flag, fix_pop=False,
+                                      deep_space={}, map_type=args.map_type)
+
+        galaxy = Galaxy(min_btn=11, max_jump=4)
+        galaxy.read_sectors(readparms)
+
+        expected_allegiances = ['As', 'AsMw', 'AsSc', 'AsT0', 'AsT1', 'AsT2', 'AsT3', 'AsT4', 'AsT5', 'AsT6', 'AsT7',
+                                'AsT8', 'AsT9', 'AsTv', 'AsVc', 'AsWc', 'AsXX', 'BlSo', 'CsIm', 'CsZh', 'FlLe', 'GlEm',
+                                'Im', 'ImDd', 'NaDr', 'NaHu', 'NaXX', 'SeFo', 'StCl']
+        actual_allegiances = list(galaxy.alg.keys())
+
+        self.assertEqual(expected_allegiances, actual_allegiances, "Allegiance name list unexpected")
+
+    def test_allegiance_list_on_sector_dictionary_read(self):
+        sourcefile = self.unpack_filename('DeltaFiles/duplicate_node_blowup/Trojan Reach.sec')
+
+        sector = SectorDictionary.load_traveller_map_file(sourcefile)
+
+        expected_allegiances = ['As', 'AsMw', 'AsSc', 'AsT0', 'AsT1', 'AsT2', 'AsT3', 'AsT4', 'AsT5', 'AsT6', 'AsT7',
+                                'AsT8', 'AsT9', 'AsTv', 'AsVc', 'AsWc', 'AsXX', 'BlSo', 'CsIm', 'CsZh', 'FlLe', 'GlEm',
+                                'Im', 'ImDd', 'NaDr', 'NaHu', 'NaXX', 'SeFo', 'StCl']
+        actual_allegiances = list(sector.allegiances.keys())
+
+        self.assertEqual(expected_allegiances, actual_allegiances, "Allegiance name list unexpected")
