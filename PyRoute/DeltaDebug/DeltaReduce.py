9928a945a705209cc5b365b23f58299899a274ba
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-05-07 15:30:50 +1000

    Add and hook up trade zone reduction

--- PyRoute/DeltaDebug/DeltaReduce.py
+++ PyRoute/DeltaDebug/DeltaReduce.py
@@ -30,6 +30,7 @@ from PyRoute.DeltaPasses.TradeCodeLineReduce import TradeCodeLineReduce
 from PyRoute.DeltaPasses.TradeCodeTrimLineReduce import TradeCodeTrimLineReduce
 from PyRoute.DeltaPasses.TwoLineReducer import TwoLineReducer
 from PyRoute.DeltaPasses.WidenHoleReducer import WidenHoleReducer
+from PyRoute.DeltaPasses.ZoneLineReduce import ZoneLineReduce
 from PyRoute.Outputs.ClassicModePDFSectorMap import ClassicModePDFSectorMap
 from PyRoute.Outputs.DarkModePDFSectorMap import DarkModePDFSectorMap
 from PyRoute.Outputs.LightModePDFSectorMap import LightModePDFSectorMap
@@ -55,7 +56,7 @@ class DeltaReduce:
         self.withinline = [IdentityLineReduce(self), Canonicalisation(self), FullLineReduce(self),
                            ImportanceLineReduce(self), CapitalLineReduce(self), AuxiliaryLineReduce(self),
                            TradeCodeLineReduce(self), TradeCodeTrimLineReduce(self), NBZLineReduce(self),
-                           BaseLineReduce(self), NoblesTrimLineReduce(self)]
+                           BaseLineReduce(self), ZoneLineReduce(self), NoblesTrimLineReduce(self)]
         self.sector_reducer = SectorReducer(self)
         self.allegiance_reducer = AllegianceReducer(self)
         self.subsector_reducer = SubsectorReducer(self)
---
+++ PyRoute/DeltaPasses/ZoneLineReduce.py
@@ -0,0 +1,29 @@
+"""
+Created on May 07, 2025
+
+@author: CyberiaResurrection
+"""
+from PyRoute.DeltaPasses.WithinLineReducer import WithinLineReducer
+from PyRoute.DeltaStar import DeltaStar
+
+
+class ZoneLineReduce(WithinLineReducer):
+
+    def _build_subs_list(self):
+        self.full_msg = "Reduction found with zone reduction"
+        self.start_msg = "Commencing zone within-line reduction"
+
+        # build substitution list - reduce _everything_
+        subs_list = []
+        segment = []
+        num_lines = len(self.reducer.sectors.lines)
+        for line in self.reducer.sectors.lines:
+            canon = DeltaStar.reduce(line, drop_trade_zone=True)
+            assert isinstance(canon,
+                              str), "Candidate line " + line + " was not reduced to a string.  Got " + canon + " instead."
+            # Skip already-reduced lines
+            if 2 < num_lines and line.startswith(canon):
+                continue
+            subs_list.append((line, canon))
+            segment.append(line)
+        return segment, subs_list
