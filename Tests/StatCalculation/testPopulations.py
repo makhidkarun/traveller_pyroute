70acf051946edae52081c1e605a0f8b804029265
Alex Goodwin <cyberia.resurrection@gmail.com>
2025-12-03 01:39:01 +1000

    Squash timeout mutants in StatCalculation class

---
+++ Tests/StatCalculation/testPopulations.py
@@ -0,0 +1,17 @@
+"""
+Created on Dec 03, 2025
+
+@author: CyberiaResurrection
+"""
+from PyRoute.StatCalculation import Populations
+from Tests.baseTest import baseTest
+
+
+class testPopulations(baseTest):
+
+    def test_init(self) -> None:
+        pop = Populations()
+        self.assertEqual("", pop.code)
+        self.assertEqual([], pop.homeworlds)
+        self.assertEqual(0, pop.count)
+        self.assertEqual(0, pop.population)
